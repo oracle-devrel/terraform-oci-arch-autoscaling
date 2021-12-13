24eeaddca8bfd7d563d5834f9c10e503d9fdc91b
lfeldman <lukasz.feldman@oracle.com>
2021-12-13 13:14:56 +0100

    Enabled ADB Private Endpoint for ORM (schema.yaml)

--- orm/variables.tf
+++ orm/variables.tf
@@ -20,7 +20,7 @@ variable "availability_domain_number" {
 
 variable "release" {
   description = "Reference Architecture Release (OCI Architecture Center)"
-  default     = "1.4.1"
+  default     = "1.4.2"
 }
 
 variable "use_existing_vcn" {
